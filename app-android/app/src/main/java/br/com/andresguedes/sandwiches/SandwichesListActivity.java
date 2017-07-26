247c4c5a070db6909211ca0bc298f1c334d3a5c5
andreguedes <andresguedes@live.com>
2017-07-26 00:33:50 -0300

    Adicionando o servidor com modificacoes e projeto Android

---
+++ app-android/app/src/main/java/br/com/andresguedes/sandwiches/SandwichesListActivity.java
@@ -0,0 +1,14 @@
+package br.com.andresguedes.sandwiches;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SandwichesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sandwiches_list);
+    }
+
+}
